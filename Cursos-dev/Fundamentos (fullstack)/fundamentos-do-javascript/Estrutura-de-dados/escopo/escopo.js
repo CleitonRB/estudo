d780984175c31b0b2b5a01c27fe935aa53aa58a6
CleitonRB <cleiton619@outlook.com>
2026-02-03 16:51:50 -0300

    lidando com retorno de funções e escopo

---
+++ Cursos-dev/Fundamentos (fullstack)/fundamentos-do-javascript/Estrutura-de-dados/escopo/escopo.js
@@ -0,0 +1,49 @@
+let pokemon = "charmander"
+
+function evoluir() {
+    pokemon = "charmeleon"
+}
+
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+
+function criarAnimal() {
+    let animal = "gato"
+}
+
+//criarAnimal()
+//console.log(animal)
+
+
+function avaliarNota(nota) {
+    if (nota > 60) {
+        var aprovado = true
+        let situacao = "aprovado"
+    } else {
+        var aprovado = false
+        let situacao = "reprovado"
+    }
+
+    console.log(nota)
+    console.log(aprovado)
+    console.log(situacao)
+}
+
+//avaliarNota(83)
+//avaliarNota(49)
+
+function ola() {
+    var texto = "Olá, Mundo!"
+}
+
+//console.log(texto)
+
+console.log(nome)
+//console.log(sobrenome)
+
+var nome = "Cleiton"
+let sobrenome = "Rodrigo"
+
+console.log(nome)
+console.log(sobrenome)
